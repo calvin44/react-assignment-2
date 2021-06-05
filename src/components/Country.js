f2722633bd7cd4cbbc9cebdf5c5502ceb14c8733
Calvin Fredicson <calvin.fredicson@gmail.com>
2021-06-05 20:11:51 +0800

    remove unused import

--- src/components/Country.js
+++ src/components/Country.js
@@ -8,7 +8,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { withStyles, createStyles, Button } from '@material-ui/core'
-import { Redirect } from 'react-router-dom'
 import styles from 'styled-components'
 
 const StyledTableContainer = withStyles((theme) => createStyles({
